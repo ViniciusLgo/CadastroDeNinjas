1220682dd23243b8458d26f42f77b57141c231e3
vinicius <vinicruzlago@gmail.com>
2026-06-24 01:13:36 -0300

    feat(api): adiciona endpoints basicos
    Refs #5
    
    Refs #6

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/Controller/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/Controller/MissoesController.java
@@ -1,8 +1,6 @@
 package dev.java10x.CadastroDeNinjas.Missoes.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -13,4 +11,42 @@ public class MissoesController {
         System.out.println("Bem vindo");
         return "Bem vindo";
     }
+
+
+
+    // add missoes, alterar, procurar, mostrar e excluir ninjas endpoint
+    @PostMapping("/criarMissoes")
+    public String criarMissoes() {
+        System.out.println("Criando");
+        return "Criando2";
+    }
+
+    @GetMapping("/todasMissoes")
+    public String mostrarTodasMissoes() {
+        System.out.println("Mostrando");
+        return "Mostrando";
+    }
+
+    //mostrar por ID
+    @GetMapping("/todasMissoesID")
+    public String mostrarTodasMissoesID() {
+        System.out.println("mostrar Todos Os Ninjas Por ID");
+        return "02 mostrar Todos Os Ninjas Por ID ";
+    }
+
+    //alterar dados (UPDATE)
+    @PutMapping("/alterarMissoesID")
+    public String alterarMissoesID() {
+        System.out.println("Alterando");
+        return "Alterando";
+    }
+
+    @DeleteMapping("/deletarMissoesID")
+    public String deletarMissoesID() {
+        System.out.println("Deletando");
+        return "Deletando";
+    }
+
+
+
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Controller/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Controller/NinjaController.java
@@ -1,6 +1,9 @@
 package dev.java10x.CadastroDeNinjas.Ninjas.Controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +15,38 @@ public class NinjaController {
     public String boasVindas() {
         System.out.println("Bem vindo");
         return "Bem vindo";
+    }
+
+    // add ninjas, alterar, procurar, mostrar e excluir ninjas endpoint
+    @PostMapping("/criar")
+    public String criar() {
+        System.out.println("Criando");
+        return "Criando2";
+    }
+
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas() {
+        System.out.println("Mostrando");
+        return "Mostrando";
+    }
+
+    //mostrar por ID
+    @GetMapping("/todosID")
+    public String mostrarTodosOsNinjasPorID() {
+        System.out.println("mostrar Todos Os Ninjas Por ID");
+        return "02 mostrar Todos Os Ninjas Por ID ";
+    }
+
+    //alterar dados (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarID() {
+        System.out.println("Alterando");
+        return "Alterando";
+    }
+
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorID() {
+        System.out.println("Deletando");
+        return "Deletando";
     }
 }
